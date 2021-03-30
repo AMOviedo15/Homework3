e7cf08bf344299c43e672af55a718feda0568180
aaronoviedo <AMOviedo15@gmail.com>
2021-03-30 00:58:59 -0500

    x

--- Zylabs10-17.py
+++ Zylabs10-17.py
@@ -1,1 +1,39 @@
+# Aaron Oviedo 1990958
 
+class ItemToPurchase:
+    def __init__(self):
+        self.item_name = "none"
+        self.item_price = 0
+        self.item_quantity = 0
+
+    def print_item_cost(self):
+        print(self.item_name + " " + str(self.item_quantity) + " @ $" + str(self.item_price) + " = $" +
+              str(self.item_price * self.item_quantity))
+
+
+if __name__ == '__main__':
+    print("Item 1")
+    useritem1 = ItemToPurchase()
+    print('Enter the item name:')
+    useritem1.item_name = input()
+    print('Enter the item price:')
+    useritem1.item_price = int(input())
+    print('Enter the item quantity:')
+    useritem1.item_quantity = int(input())
+
+    print("\nItem 2")
+    useritem2 = ItemToPurchase()
+    print('Enter the item name:')
+    useritem2.item_name = input()
+    print('Enter the item price:')
+    useritem2.item_price = int(input())
+    print('Enter the item quantity:')
+    useritem2.item_quantity = int(input())
+
+    print("\nTOTAL COST")
+    useritem1.print_item_cost()
+    useritem2.print_item_cost()
+
+    total = (useritem1.item_price * useritem1.item_quantity) + (useritem2.item_price * useritem2.item_quantity)
+
+    print("\nTotal: $" + str(total))
--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 # Aaron Oviedo 1990958
 
-class Team:
+class ItemToPurchase:
     def __init__(self):
-        self.team_name = 'None'
-        self.team_wins = 0
-        self.team_losses = 0
+        self.item_name = "none"
+        self.item_price = 0
+        self.item_quantity = 0
 
-    def set_team_name(self, name):
-        self.team_name = name
-
-    def set_team_wins(self, wins):
-        self.team_wins = wins
-
-    def set_team_losses(self, losses):
-        self.team_losses = losses
-
-    def get_win_percentage(self):
-        return self.team_wins / (self.team_wins + self.team_losses)
+    def print_item_cost(self):
+        print(self.item_name + " " + str(self.item_quantity) + " @ $" + str(self.item_price) + " = $" +
+              str(self.item_price * self.item_quantity))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    print("Item 1")
+    useritem1 = ItemToPurchase()
+    print('Enter the item name:')
+    useritem1.item_name = input()
+    print('Enter the item price:')
+    useritem1.item_price = int(input())
+    print('Enter the item quantity:')
+    useritem1.item_quantity = int(input())
 
-    team = Team()
+    print("\nItem 2")
+    useritem2 = ItemToPurchase()
+    print('Enter the item name:')
+    useritem2.item_name = input()
+    print('Enter the item price:')
+    useritem2.item_price = int(input())
+    print('Enter the item quantity:')
+    useritem2.item_quantity = int(input())
 
-    team_name = input()
-    team_wins = int(input())
-    team_losses = int(input())
+    print("\nTOTAL COST")
+    useritem1.print_item_cost()
+    useritem2.print_item_cost()
 
-    team.set_team_name(team_name)
-    team.set_team_wins(team_wins)
-    team.set_team_losses(team_losses)
+    total = (useritem1.item_price * useritem1.item_quantity) + (useritem2.item_price * useritem2.item_quantity)
 
-    if team.get_win_percentage() >= 0.5:
-        print('Congratulations, Team', team.team_name, 'has a winning average!')
-    else:
-        print('Team', team.team_name, 'has a losing average.')
+    print("\nTotal: $" + str(total))
