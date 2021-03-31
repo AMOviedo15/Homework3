61770aff63034aa36d332175115aaaf268daebbb
AMOviedo15 <77904077+AMOviedo15@users.noreply.github.com>
2021-03-30 19:32:51 -0500

    Update Zylabs11-18.py

--- Zylabs11-18.py
+++ Zylabs11-18.py
@@ -1,1 +1,13 @@
+# Aaron Oviedo 1990958
 
+list = input().split()
+numlist = []
+
+for number in list:
+    number = int(number)
+    if number >= 0:
+        numlist.append(number)
+
+numlist.sort()
+for number in numlist:
+    print(number, end = ' ')
